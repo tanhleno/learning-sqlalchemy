9473a8453700957b3930483c676ea9455c01579d
Tanhleno Teixeira <tts.tanhleno@gmail.com>
2022-07-29 11:57:27 -0300

    Test transaction rollback

--- main.py
+++ main.py
@@ -3,6 +3,19 @@ from sqlalchemy import text
 
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True, future=True)
+with engine.begin() as conn:
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
 with engine.connect() as conn:
-    result = conn.execute(text("SELECT 'Hello World'"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}]
+    )
+    conn.commit()
+with engine.connect() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        {"x": 2, "y": 2}
+    )
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT * FROM some_table"))
     print(result.all())
